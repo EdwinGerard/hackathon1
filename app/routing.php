ea71357fde7c6da59c59c7213bdabf923033b063
Yoan BEAUCHAMP <yoan.beauchamp@gmail.com>
2018-04-05 17:34:44 +0200

    mise en place de la nav et d'une modal de connexion

--- app/routing.php
+++ app/routing.php
@@ -10,10 +10,13 @@
  */
 
 $routes = [
-    'Item' => [ // Controller
+    /*'Item' => [ // Controller
+         ['index', '/', 'GET'], // action, url, method
+         ['add', '/item/add', 'GET'], // action, url, method
+         ['edit', '/item/edit/{id:\d+}', 'GET'], // action, url, method
+         ['show', '/item/{id:\d+}', 'GET'], // action, url, method
+     ],*/
+    'Home' => [
         ['index', '/', 'GET'], // action, url, method
-        ['add', '/item/add', 'GET'], // action, url, method
-        ['edit', '/item/edit/{id:\d+}', 'GET'], // action, url, method
-        ['show', '/item/{id:\d+}', 'GET'], // action, url, method
     ],
 ];
