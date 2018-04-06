5e79d4ba3a38005cb9c3355d91c8e7dd97c18dd1
SteadyB_Benjamin <benjamin.berthier.mail@gmail.com>
2018-04-06 07:33:08 +0200

    fix routing virgule manquante

--- app/routing.php
+++ app/routing.php
@@ -24,7 +24,7 @@ $routes = [
     'Games' => [
         ['games', '/games',['GET', 'POST']], // action, url, method
         ['joinGame','/proc/join_game',['GET', 'POST']],
-        ['addGame','/proc/addGame',['GET', 'POST']]
+        ['addGame','/proc/addGame',['GET', 'POST']],
         ['myGame','/myGame',['GET', 'POST']],
     ],
 
